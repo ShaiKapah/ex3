c8f326a13529c7099e43c1fada443e64ff7698bc
ShaiKapah <shaikapah09@gmail.com>
2025-11-29 20:02:58 +0200

    Implement CheckSequenceOfThree function
    Added CheckSequenceOfThree function to evaluate sequences.

--- ex3.c
+++ ex3.c
@@ -46,6 +46,8 @@ void printBoard(char[][COLS], int, int);
 
 int getPlayerType(int);
 
+int CheckSequenceOfThree(char[][COLS], char[COLS], int, int, int, int, char);
+
 
 int main() {
     char board[ROWS][COLS];
@@ -56,6 +58,12 @@ int main() {
     printBoard(board, ROWS, COLS);
     runConnectFour(board, ROWS, COLS, p1Type, p2Type);
     return 0;
+}
+void initBoard(char board[][COLS], int rows, int cols) {
+    for (int r = 0; r < rows; r++) 
+        for (int c = 0; c < cols; c++) {
+            board[r][c] = EMPTY;
+        }
 }
 
 void printBoard(char board[][COLS], int rows, int cols) {
@@ -91,3 +99,208 @@ int getPlayerType(int playerNumber) {
         while (getchar() != '\n'); // clear rest of input
     }
 }
+int makeMove(char board[][COLS], int ROWS, int COLS, int col, char token){
+    if (col < 0 || col >= COLS) 
+        return -1;
+    int row = getFreeRow(board, ROWS, COLS, col);
+    if (row == -1) 
+        return -1;
+    board[row][col] = token;
+    return row;
+}
+void runConnectFour(char board[][COLS], int ROWS, int COLS, int p1Type, int p2Type){
+    int PlayerNumber = 1;
+    int ptype;
+    char token;
+    while(1){
+        token = (PlayerNumber == 1) ? TOKEN_P1 : TOKEN_P2;
+        ptype = (PlayerNumber == 1) ? p1Type : p2Type;
+        printf("Player %d (%c) turn.\n", PlayerNumber, token);
+
+        int ChosenColumn = -1;
+        if(ptype == HUMAN){
+            ChosenColumn = humanChoose(board,ROWS,COLS);
+        }
+        else{
+            ChosenColumn = computerChoose(board,ROWS,COLS,token,(token == TOKEN_P1) ? TOKEN_P2 : TOKEN_P1);
+            printf("Computer chose column %d\n", ChosenColumn);
+        }
+
+        int RowChanged = makeMove(board,ROWS,COLS,ChosenColumn,token);
+        printBoard(board,ROWS,COLS);
+
+        if(checkVictory(board,ROWS,COLS,RowChanged,ChosenColumn,token)){
+            printf("Player %d (%c) wins!",PlayerNumber, token);
+            break;
+        }
+        if(isBoardFull(board,ROWS,COLS)){
+            printf("Board full and no winner. It's a tie!");
+            break;
+        }
+        PlayerNumber = (PlayerNumber == 1) ? 2 : 1;
+    }
+}
+int humanChoose(char board[][COLS], int ROWS, int COLS) {
+     int col = 0;
+    char c;
+
+    while(1) {
+        printf("Enter column (1-%d): ", COLS);
+        col = 0;
+
+        do {
+            scanf("%c", &c);
+        } while(c == ' ' || c == '\t' || c == '\n');
+
+        while(c >= '0' && c <= '9') {
+            col = col * 10 + (c - '0');
+            scanf("%c", &c);
+        }
+
+        if(col < 1 || col > COLS) {
+            printf("Invalid column. Choose between 1 and %d.\n", COLS);
+            continue;
+        }
+
+        if(isColumnFull(board, ROWS, COLS, col - 1)) {
+            printf("Column %d is full. Choose another column.\n", col);
+            continue;
+        }
+
+        break;
+    }
+
+    return col - 1; 
+}
+int computerChoose(char board[][COLS], int ROWS, int COLS, char mytoken, char othertoken){
+    int OrderRule[COLS];
+    int centerLeft,centerRight;
+    if(COLS%2){
+        int center = COLS/2;
+        int i = 0;
+        OrderRule[i++] = center;
+        for(int d = 1; i < COLS; d++){
+            int left = center - d;
+            int right = center + d;
+            if(left >= 0)
+                OrderRule[i++] = left;
+            if(right < COLS)
+                OrderRule[i++] = right;
+        }
+    }
+    else{
+        int cl = COLS/2 -1;
+        int cr = COLS/2;
+        int i = 0;
+        OrderRule[i++] = cl;
+        OrderRule[i++] = cr;
+        for(int d = 1; i < COLS; d++){
+            int left = cl - d;
+            int right = cr + d;
+            if(left >= 0)
+                OrderRule[i++] = left;
+            if(right < COLS)
+                OrderRule[i++] = right;
+        }  
+    }
+    //check winning move
+    for(int i = 0; i < COLS; i++){
+        int c = OrderRule[i];
+        if(isColumnFull(board,ROWS,COLS,c))
+            continue;
+        int r = getFreeRow(board,ROWS,COLS,c);
+        board[r][c] = mytoken;
+        int win = checkVictory(board,ROWS,COLS,r,c,mytoken);
+        board[r][c] = EMPTY;
+        if(win)
+            return c;
+    }
+    //block op win
+    for(int i = 0; i < COLS; i++){
+        int c = OrderRule[i];
+        if(isColumnFull(board,ROWS,COLS,c))
+            continue;
+        int r = getFreeRow(board,ROWS,COLS,c);
+        board[r][c] = othertoken;
+        int win = checkVictory(board,ROWS,COLS,r,c,othertoken);
+        board[r][c] = EMPTY;
+        if(win)
+            return c;
+    }
+    //create 3
+    for(int i = 0; i < COLS; i++){
+        int c = OrderRule[i];
+        if(isColumnFull(board,ROWS,COLS,c))
+            continue;
+        int r = getFreeRow(board,ROWS,COLS,c);
+        board[r][c] = mytoken;
+        int maxseq = CheckSequenceOfThree(board, OrderRule, ROWS, COLS, r, c, mytoken);
+        board[r][c] = EMPTY;
+        if(maxseq)
+            return c;
+    }
+    //block 3
+     for(int i = 0; i < COLS; i++){
+        int c = OrderRule[i];
+        if(isColumnFull(board,ROWS,COLS,c))
+            continue;
+        int r = getFreeRow(board,ROWS,COLS,c);
+        board[r][c] = othertoken;
+        int maxseq = CheckSequenceOfThree(board, OrderRule, ROWS, COLS, r, c, othertoken);
+        board[r][c] = EMPTY;
+        if(maxseq)
+            return c;
+    }
+
+    for(int i = 0; i<COLS; i++){
+        int c = OrderRule[i];
+        if(!isColumnFull(board,ROWS,COLS,c))
+            return c;
+    }
+    return 0;
+}
+    int checkVictory(char board[][COLS], int ROWS, int COLS, int lastRow, int lastCol, char token) {
+     if (lastRow < 0 || lastCol < 0) return 0;
+    const int moveoptions[4][2] = {{0, 1},{1, 0}, {1, 1},{1, -1}};
+
+    for (int d = 0; d < 4; d++) {
+        int dr = moveoptions[d][0], dc = moveoptions[d][1];
+        int count = 1; 
+
+        int r = lastRow + dr, c = lastCol + dc;
+        while (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == token) {
+            count++; r += dr; c += dc;
+        }
+        
+        r = lastRow - dr; c = lastCol - dc;
+        while (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == token) {
+            count++; r -= dr; c -= dc;
+        }
+
+        if (count >= CONNECT_N) return 1;
+    }
+    return 0;
+}
+int CheckSequenceOfThree(char board[][COLS], char OrderRule[COLS], int ROWS, int COLS, int lastRow, int lastCol, char token){
+    if (lastRow < 0 || lastCol < 0) return 0;
+    const int moveoptions[4][2] = {{0, 1},{1, 0}, {1, 1},{1, -1}};
+
+    for (int d = 0; d < 4; d++) {
+        int dr = moveoptions[d][0], dc = moveoptions[d][1];
+        int count = 1; 
+
+        int r = lastRow + dr, c = lastCol + dc;
+        while (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == token) {
+            count++; r += dr; c += dc;
+        }
+        
+        r = lastRow - dr; c = lastCol - dc;
+        while (r >= 0 && r < ROWS && c >= 0 && c < COLS && board[r][c] == token) {
+            count++; r -= dr; c -= dc;
+        }
+
+        if (count == 3) return 1;
+    }
+    return 0;
+} 
+
